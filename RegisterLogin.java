dc7f70ab6e11a273f1901e7673fcf7ccd2eeb7e8
winstongama17-dot <winstongama17@gmail.com>
2026-04-13 20:55:23 +0200

    Add files via upload

---
+++ RegisterLogin.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.registerlogin;
+
+/**
+ *
+ * @author Student
+ */
+public class RegisterLogin {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
